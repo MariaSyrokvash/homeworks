359ae0a5dfc2c233b6e479f3975ab413635a5023
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-10-26 15:30:28 +0300

    Updates

---
+++ precache-manifest.103a433f0ee334f8a6a9370f2213a590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c947e7666aa3938e34e7cdb5d7f8d7",
+    "url": "/MariaSyrokvash/homeworks.git/index.html"
+  },
+  {
+    "revision": "bd35603742fb2e720dd7",
+    "url": "/MariaSyrokvash/homeworks.git/static/css/main.90bbe944.chunk.css"
+  },
+  {
+    "revision": "f7b9ca8a21f9cfcd02f4",
+    "url": "/MariaSyrokvash/homeworks.git/static/js/2.23d4b5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MariaSyrokvash/homeworks.git/static/js/2.23d4b5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd35603742fb2e720dd7",
+    "url": "/MariaSyrokvash/homeworks.git/static/js/main.9364ce17.chunk.js"
+  },
+  {
+    "revision": "f53d1b7fdb4a590a70b3",
+    "url": "/MariaSyrokvash/homeworks.git/static/js/runtime-main.ab029152.js"
+  }
+]);
