4ba4a07be48649116d764d4f440c00a3d00ed230
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-11-06 01:12:57 +0300

    Updates

---
+++ precache-manifest.4a45acbb5abefafbbd2a6048a419015c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86155fabd93c56eb37ee1cc7818b26d",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "59b71a0ea8c33ad8502e",
+    "url": "/homeworks/static/css/main.62e38bf0.chunk.css"
+  },
+  {
+    "revision": "7fc12792e3f954829aaf",
+    "url": "/homeworks/static/js/2.e1401cf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homeworks/static/js/2.e1401cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b71a0ea8c33ad8502e",
+    "url": "/homeworks/static/js/main.8e68d124.chunk.js"
+  },
+  {
+    "revision": "9263b9c20382da27b976",
+    "url": "/homeworks/static/js/runtime-main.1f7d4fd3.js"
+  }
+]);
