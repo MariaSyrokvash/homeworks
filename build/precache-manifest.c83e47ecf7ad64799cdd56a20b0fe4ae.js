2ed620a0443269e80d25f50440de8e3192bd0051
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-11-25 00:07:20 +0300

    feat: add deploy

---
+++ build/precache-manifest.c83e47ecf7ad64799cdd56a20b0fe4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4750cd6a5d7f34b8e5bb8c987d79f3e1",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "61ec09a50723751b72f8",
+    "url": "/homeworks/static/css/main.fa36644a.chunk.css"
+  },
+  {
+    "revision": "39255b1d143136d85b7d",
+    "url": "/homeworks/static/js/2.b2564881.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homeworks/static/js/2.b2564881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ec09a50723751b72f8",
+    "url": "/homeworks/static/js/main.64c0b58f.chunk.js"
+  },
+  {
+    "revision": "9263b9c20382da27b976",
+    "url": "/homeworks/static/js/runtime-main.1f7d4fd3.js"
+  }
+]);
