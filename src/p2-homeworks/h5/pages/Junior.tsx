ee9a9a1b324f39f658122e9ea70144f849870df8
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-11-28 23:16:55 +0300

    feat: add 11hw

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import hw from '../../h2/HW2.module.css';
+import HW11 from '../../h11/HW11';
 
 
 function Junior() {
 	return (
 		<div>
 			<p className={hw.title}>Homeworks 6</p>
-
-			Junior
+			<HW11/>
 		</div>
 	);
 }
