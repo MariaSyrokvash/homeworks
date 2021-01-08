8b2becdcc6cd8f3a1fc884cf9a0a994f2fd39768
MariaSyrokvash <mariya.syrokvash@gmail.com>
2021-01-09 01:10:43 +0300

    feat: add hw-12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,26 +1,34 @@
-import React from "react";
-import s from "./HW12.module.css";
+import React from 'react';
+import s from './HW12.module.css';
+import SuperSelect from '../h7/common/c5-SuperSelect/SuperSelect';
+import '../h7/common/c5-SuperSelect/SuperSelect.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {setThemeAC} from '../h10/bll/themeReducer';
+import {AppStoreType} from '../h10/bll/store';
 
-const themes = ['dark', 'red', 'some'];
+const themes = ['dark', 'red', 'green'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+  const theme = useSelector<AppStoreType, string>(state => state.theme.color)
+	const dispatch = useDispatch()
+  const onChangeCallback = (theme: string) => dispatch(setThemeAC(theme))
 
-    // useDispatch, onChangeCallback
+	return (
+		<div className={s.box}>
+			<div className={`${s[theme]} ${s.wrapper}`}>
+        <p className='hw'>Homeworks 12</p>
+				<span className={s[theme + '-text']}></span>
 
-    return (
-        <div className={s[theme]}>
-            <hr/>
-            <span className={s[theme + '-text']}>
-                homeworks 12
-            </span>
 
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
-
-            <hr/>
-        </div>
-    );
+				<SuperSelect
+          options={themes}
+          onChangeOption={onChangeCallback}
+          value={theme}
+        />
+			</div>
+	 </div>
+	);
 }
 
 export default HW12;
+
